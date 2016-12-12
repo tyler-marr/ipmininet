623c31a3d6e7757fd548041774d8dd11946e1854
Olivier Tilmans <olivier.tilmans@uclouvain.be>
2016-12-12 12:26:27 +0100

    Added release script
    Signed-off-by: Olivier Tilmans <olivier.tilmans@uclouvain.be>

---
+++ make_pypi_release.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+box () {
+    printf "\n\e[1;31;40m@@@ $1\e[0m\n\n"
+}
+
+box "Executing tests"
+py.test || exit 1
+
+box "Packaging the source"
+python setup.py sdist || exit 1
+
+if [ "$1" = "push" ]; then
+    box "Uploading to Pypi"
+    twine upload dist/*
+else
+    box "Uploading to TestPypi"
+    twine upload dist/* -r testpypi
+fi
