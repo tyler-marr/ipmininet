46e8ab44679b28cc721195ccda7d85d9cb156373
Mathieu Jadin <mathjadin@gmail.com>
2019-03-29 11:31:58 +0100

    Mark Python3 compatibility

--- setup.py
+++ setup.py
@@ -24,7 +24,11 @@ setup(
         "Intended Audience :: Developers",
         "Topic :: System :: Networking",
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7'
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
         ],
     keywords='networking OSPF IP BGP quagga mininet',
     license='GPLv2',
