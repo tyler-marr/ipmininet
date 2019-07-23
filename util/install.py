f3c14cb0e62c1a9b7197d8b828cc5b096495ae81
Mathieu Jadin <mathjadin@gmail.com>
2019-07-24 01:12:49 +0200

    Install FRRouting instead of Quagga

--- util/install.py
+++ util/install.py
@@ -7,10 +7,11 @@ import re
 from utils import supported_distributions, identify_distribution, sh
 
 MininetVersion = "master"
-QuaggaVersion = "1.2.4"
+FRRoutingVersion = "7.1"
 OpenrRelease = "rc-20190419-11514"
 
 os.environ["PATH"] = "%s:/sbin:/usr/sbin/:/usr/local/sbin" % os.environ["PATH"]
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Install IPMininet with its dependencies")
@@ -20,7 +21,7 @@ def parse_args():
     parser.add_argument("-m", "--install-mininet", help="Install the last version of mininet and its dependencies",
                         action="store_true")
     parser.add_argument("-a", "--all", help="Install all daemons", action="store_true")
-    parser.add_argument("-q", "--install-quagga", help="Install Quagga (version %s) daemons" % QuaggaVersion,
+    parser.add_argument("-q", "--install-frrouting", help="Install FRRouting (version %s) daemons" % FRRoutingVersion,
                         action="store_true")
     parser.add_argument("-r", "--install-radvd", help="Install the RADVD daemon",
                         action="store_true")
@@ -52,39 +53,77 @@ def install_mininet():
        cwd=args.output_dir)
 
 
-def install_quagga():
-    dist.install("autoconf", "automake", "libtool", "make", "gcc", "gawk", "pkg-config")
+def install_libyang():
+
+    packages = []
 
     if dist.NAME == "Ubuntu" or dist.NAME == "Debian":
-        dist.install("libreadline-dev", "libc-ares-dev")
+        dist.install("libpcre16-3", "libpcre3-dev", "libpcre32-3", "libpcrecpp0v5")
+        cmd = "dpkg -i"
+        libyang_url = "https://ci1.netdef.org/artifact/LIBYANG-YANGRELEASE" \
+                      "/shared/build-10/Debian-AMD64-Packages"
+        packages.extend(["libyang0.16_0.16.105-1_amd64.deb",
+                         "libyang-dev_0.16.105-1_amd64.deb"])
     elif dist.NAME == "Fedora":
-        dist.install("readline-devel", "c-ares-devel")
+        dist.install("pcre-devel")
+        cmd = "rpm -ivh"
+        libyang_url = "https://ci1.netdef.org/artifact/LIBYANG-YANGRELEASE" \
+                      "/shared/build-10/Fedora-29-x86_64-Packages/"
+        packages.extend(["libyang-0.16.111-0.x86_64.rpm",
+                         "libyang-devel-0.16.111-0.x86_64.rpm"])
+    else:
+        return
 
-    quagga_src = os.path.join(args.output_dir, "quagga-%s" % QuaggaVersion)
-    quagga_tar = quagga_src + ".tar.gz"
-    sh("wget http://download.savannah.gnu.org/releases/quagga/quagga-%s.tar.gz" % QuaggaVersion,
-       "tar -zxvf '%s'" % quagga_tar,
+    for package in packages:
+        sh("wget %s/%s" % (libyang_url, package),
+           "%s %s" % (cmd, package),
+           "rm %s" % package,
+           cwd=args.output_dir)
+
+
+def install_frrouting():
+    dist.install("autoconf", "automake", "libtool", "make", "gcc", "groff",
+                 "patch", "make", "bison", "flex", "gawk",
+                 "python3-pytest")
+
+    if dist.NAME == "Ubuntu" or dist.NAME == "Debian":
+        dist.install("libreadline-dev", "libc-ares-dev", "libjson-c-dev",
+                     "perl", "python3-dev", "libpam0g-dev", "libsystemd-dev",
+                     "libsnmp-dev", "pkg-config")
+    elif dist.NAME == "Fedora":
+        dist.install("readline-devel", "c-ares-devel", "json-c-devel",
+                     "perl-core", "python3-devel", "pam-devel", "systemd-devel",
+                     "net-snmp-devel", "pkgconfig")
+
+    install_libyang()
+
+    frrouting_src = os.path.join(args.output_dir, "frr-%s" % FRRoutingVersion)
+    frrouting_tar = frrouting_src + ".tar.gz"
+    sh("wget https://github.com/FRRouting/frr/releases/download/frr-{v}/frr-{v}.tar.gz".format(v=FRRoutingVersion),
+       "tar -zxvf '%s'" % frrouting_tar,
        cwd=args.output_dir)
 
-    quagga_install = os.path.join(args.output_dir, "quagga")
-    sh("./configure '--prefix=%s'" % quagga_install,
+    frrouting_install = os.path.join(args.output_dir, "frr")
+    sh("./configure '--prefix=%s'" % frrouting_install,
        "make",
        "make install",
-       cwd=quagga_src)
+       cwd=frrouting_src)
 
-    sh("rm -r '%s' '%s'" % (quagga_src, quagga_tar))
+    sh("rm -r '%s' '%s'" % (frrouting_src, frrouting_tar))
 
-    sh("groupadd quagga", may_fail=True)
-    sh("usermod -a -G quagga root", may_fail=True)
+    sh("groupadd frr", may_fail=True)
+    sh("groupadd frrvty", may_fail=True)
+    sh("usermod -a -G frr root", may_fail=True)
+    sh("usermod -a -G frrvty root", may_fail=True)
 
-    for root, _, files in os.walk(os.path.join(quagga_install, "sbin")):
+    for root, _, files in os.walk(os.path.join(frrouting_install, "sbin")):
         for f in files:
             link = os.path.join("/usr/sbin", os.path.basename(f))
             if os.path.exists(link):
                 os.remove(link)
             os.symlink(os.path.join(root, f), link)
         break
-    for root, _, files in os.walk(os.path.join(quagga_install, "bin")):
+    for root, _, files in os.walk(os.path.join(frrouting_install, "bin")):
         for f in files:
             link = os.path.join("/usr/bin", os.path.basename(f))
             if os.path.exists(link):
@@ -170,8 +209,8 @@ dist.install("python-pip", "python3-pip")
 if args.install_mininet:
     install_mininet()
 
-if args.all or args.install_quagga:
-    install_quagga()
+if args.all or args.install_frrouting:
+    install_frrouting()
 
 if args.all or args.install_radvd:
     dist.install("radvd")
