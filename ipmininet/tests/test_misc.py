bf9587f60006b48926d1b1af0ce0255c7374a861
Mathieu Jadin <mathjadin@gmail.com>
2019-08-05 11:31:21 +0200

    Add tests for utility functions

--- ipmininet/tests/test_misc.py
+++ ipmininet/tests/test_misc.py
@@ -1,8 +1,16 @@
-import pytest
-import ipaddress
-import ipmininet.utils as utils
+import os
 import subprocess
+
+import ipaddress
+import pytest
+
+import ipmininet.utils as utils
+from ipmininet.clean import cleanup
+from ipmininet.examples.static_address_network import StaticAddressNet
+from ipmininet.ipnet import IPNet
 from ipmininet.link import _parse_addresses
+from ipmininet.router.config.utils import ip_statement
+from . import require_root
 
 
 @pytest.mark.parametrize('address', [
@@ -66,3 +74,80 @@ def test_ip_address_format():
         assert a.with_prefixlen in out
     # IF status, MAC, inet, valid, inet, valid, ..., inet6, valid, ...
     assert len(out.strip('\n').split('\n')) == (2 + 2 * len(v4) + 2 * len(v6))
+
+
+@pytest.mark.parametrize("cmd,present", [
+    ("ls", True),
+    ("/bin/sh", True),
+    ("for", False),  # Never a command because in bash syntax
+    (os.path.abspath(__file__), False),
+])
+def test_require_cmd(cmd, present):
+    try:
+        utils.require_cmd(cmd)
+        assert present, "The command [%s] was found" \
+                        " while it is not present" % cmd
+    except RuntimeError:
+        assert not present, "The command [%s] could not be found" \
+                            " while it is present" % cmd
+
+
+@pytest.mark.parametrize("node,use_v4,use_v6,expected", [
+    ("h1", True, True, (u"10.0.0.2", u"2001:1a::2")),
+    ("h1", False, True, (None, u"2001:1a::2")),
+    ("h1", True, False, (u"10.0.0.2", None)),
+    ("h1", False, False, (None, None)),
+])
+@require_root
+def test_address_pair(node, use_v4, use_v6, expected):
+    try:
+        net = IPNet(topo=StaticAddressNet())
+        net.start()
+        assert utils.address_pair(net[node], use_v4, use_v6) == expected
+        net.stop()
+    finally:
+        cleanup()
+
+
+@pytest.mark.parametrize("start,node,present", [
+    ("h1", "h1", True),
+    ("h1", "r1", True),
+    ("h1", "h2", True),
+    ("h1", "h3", True),
+    ("h1", "s2", False),  # Switches are not searched
+    ("h1", "None", False),
+])
+@require_root
+def test_find_node(start, node, present):
+    try:
+        net = IPNet(topo=StaticAddressNet())
+        net.start()
+        i = utils.find_node(net[start], node)
+        if present:
+            assert i is not None, "Node %s not found from node %s" % (node, start)
+            assert i.node.name == node, "Node %s was found while we expected %s" % (i.node.name, node)
+        else:
+            assert i is None, "Node %s should not be found from node %s" % (node, start)
+        net.stop()
+    finally:
+        cleanup()
+
+
+@pytest.mark.parametrize("test_input,expected", [
+    (4, "ip"),
+    (6, "ipv6"),
+    (u"10.0.0.1", "ip"),
+    (u"10.0.0.0/8", "ip"),
+    (u"10.0.0.1/8", "ip"),
+    (u"2042::1", "ipv6"),
+    (u"2042::/16", "ipv6"),
+    (u"2042::1/16", "ipv6"),
+    (ipaddress.ip_address(u"10.0.0.1"), "ip"),
+    (ipaddress.ip_network(u"10.0.0.0/8"), "ip"),
+    (ipaddress.ip_interface(u"10.0.0.1/8"), "ip"),
+    (ipaddress.ip_address(u"2042::1"), "ipv6"),
+    (ipaddress.ip_network(u"2042::/16"), "ipv6"),
+    (ipaddress.ip_interface(u"2042::1/16"), "ipv6"),
+])
+def test_ip_statement(test_input, expected):
+    assert ip_statement(test_input) == expected
