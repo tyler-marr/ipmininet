759358fecd5d8faa7363447320a3c32a7458c631
Mathieu Jadin <mathjadin@gmail.com>
2019-09-20 10:04:37 +0200

    Cleanup: change pattern + do SIGINT before SIGKILL
    Some tests with the same name as daemon could be killed. This changes
    the pattern so that it does not happen.
    
    Moreover, it is safer for some daemon to receive a SIGINT to clean
    whatever they want before resorting to a hard SIGKILL after a few
    seconds if that did not work.

--- ipmininet/clean.py
+++ ipmininet/clean.py
@@ -1,9 +1,10 @@
+import time
+from subprocess import check_output, CalledProcessError
 
-from mininet.log import lg as log
 import mininet.clean as mnclean
+from mininet.log import lg as log
 
 import ipmininet.router.config as daemons
-
 from .utils import is_container
 
 
@@ -23,9 +24,38 @@ def cleanup(level='info'):
             killp = [killp]
         patterns.extend(killp)
     log.info('*** Cleaning up daemons:\n')
-    for pattern in patterns:
-        mnclean.killprocs('"%s"' % pattern)
+    killprocs(['"^%s"' % p for p in patterns])
     log.info('\n')
+
+
+def killprocs(patterns, timeout=10):
+    """Reliably terminate processes matching a pattern (including args)"""
+
+    # Try clean kill
+    for p in patterns:
+        mnclean.sh('pkill -SIGINT -f %s' % p)
+
+    # Make sure they are gone
+    t = 0
+    to_be_killed = {p: True for p in patterns}
+    while any(to_be_killed.values()) or t >= timeout:
+        for p in patterns:
+            try:
+                pids = check_output(['pgrep', '-f', p])
+            except CalledProcessError:
+                pids = ''
+            if not pids:
+                log.info(p)
+                to_be_killed[p] = False
+
+        time.sleep(.5)
+        t += .5
+
+    # Last resort
+    for p in patterns:
+        if to_be_killed[p]:
+            log.info(p)
+            mnclean.killprocs(p)
 
 
 if __name__ == '__main__':
